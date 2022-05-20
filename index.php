9d6e3e9a6715ee47b7d5a2a65eb781e2bf7d554d
Giuseppe <giuseppecantafio@hotmail.it>
2022-05-20 14:26:13 +0200

    Esercizio concluso

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$stringa = 'Pasta al sugo e carne al sugo';
+if(isset($_GET['variabile'])){
+    $modificatore = $_GET['variabile'];
+} else {
+    $modificatore = 'Inserire un modificatore prego';
+};
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+</head>
+<body>
+    <h2>Stringa inserita:</h2>
+    <p><?php echo $stringa; ?></p>
+    <p><?php echo 'La lunghezza della stringa è '.strlen($stringa); ?></p>
+
+    <h2>Stringa modificata:</h2>
+    <p><?php echo $nuovaStringa = str_replace($modificatore, '***', $stringa); ?></p>
+    <p><?php echo 'La lunghezza della stringa modificata è '.strlen($nuovaStringa); ?></p>
+</html>
